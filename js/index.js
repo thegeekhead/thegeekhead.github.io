a845a992acb64cc33203f3f9ebd740739413014f
Anshansh Suman <mr.anshansh@gmail.com>
2021-10-12 01:12:16 +0530

    hello world

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const titleText = document.querySelector(".blinking");
+var typed = new Typed('.typing', {
+    strings: [
+    "नमस्ते" ,
+    "Hello" ,
+	"Bonjour",
+    "Hola" ,
+    "Salve" ,
+    "Olá" ],
+    loop: true,
+    typeSpeed: 160,
+    backSpeed: 80
+  });
